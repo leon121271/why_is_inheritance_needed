4466731a8434055a03d26ef68e62097ef147a6c5
leon121271 <don.leon121271@gmail.com>
2024-12-14 18:25:27 +0300

    Add files via upload

---
+++ why_is_inheritance_needed.py
@@ -0,0 +1,48 @@
+class Animal:
+    alive = True                                # живое
+    fed = False                                 # накормленный
+    def __init__(self, name):
+        self.name = name                        # имя животного
+
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Plant:
+    edible = False                              # съедобность
+    def __init__(self, name):
+        self.name = name                        # имя растения
+
+class Mammal(Animal):
+    pass                                        # млекопитающее
+
+class Predator(Animal):
+    pass                                        # хищник
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+
+a1.eat(p1)
+a2.eat(p2)
+
+print(a1.alive)
+print(a2.fed)
+
